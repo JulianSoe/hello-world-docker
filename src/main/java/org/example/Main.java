0a476695362dd35be582ad7e7041162cfde116bf
JulianSoe <j.soe@gmx.de>
2023-12-13 12:01:54 +0100

    step added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,11 @@ import static java.lang.String.format;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+        System.out.println("Jetzt gehts wirklich los");
         int count =0;
         while (true) {
             Thread.sleep(2*1500);
-            System.out.println("Jetzt gehts los");
+
             System.out.println(format("App running for ... %d", count++));
         }
     }
