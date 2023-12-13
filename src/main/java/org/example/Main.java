0ed9c8659a76b375b3e58f9c8cee5b6d5ba27480
JulianSoe <j.soe@gmx.de>
2023-12-13 11:58:41 +0100

    step added to yaml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,12 @@ import static java.lang.String.format;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+        int a = 5;
+        System.out.println("Jetzt gehts wirklich los " + a);
         int count =0;
         while (true) {
             Thread.sleep(2*1500);
-            System.out.println("Jetzt gehts los");
+
             System.out.println(format("App running for ... %d", count++));
         }
     }
