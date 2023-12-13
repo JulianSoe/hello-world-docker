42471b80d7424beb7ac69a5abec687d7c7f0aa5f
JulianSoe <j.soe@gmx.de>
2023-12-13 09:46:49 +0100

    sleep changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         int count =0;
         while (true) {
-            Thread.sleep(2*1000);
+            Thread.sleep(2*1500);
             System.out.println("Jetzt gehts los");
             System.out.println(format("App running for ... %d", count++));
         }
